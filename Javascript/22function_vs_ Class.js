ef89919073554d587d69ebcffcc3d2dc60b75e22
thhs16 <thaehtapehtarsan@gmail.com>
2022-11-02 15:52:38 +0630

    my_js_practice

---
+++ Javascript/22function_vs_ Class.js
@@ -0,0 +1,18 @@
+
+function profile(){
+          let name="thhs";
+          let age=17;
+}
+
+// console.log(name); // We can't use the variables built in function.
+
+class Profile{
+          name ="thhs";
+          age = 20;
+}
+
+// creating object for class Profile
+let myProfile = new Profile;
+// displaying data from class Profile
+console.log(myProfile.name);
+console.log(myProfile.age);// We can use the varibles built in class
