34d9951b9a2bb422a60c935fef999af257cac7c8
thhs16 <thaehtapehtarsan@gmail.com>
2022-11-19 15:31:03 +0630

    weather_project

---
+++ Javascript/10weather.js
@@ -0,0 +1,31 @@
+let cityName = 'Yangon';
+
+const weatherInfo={
+
+          Taunggyi:{
+                    temp: '22°C',                     // *C
+                    precipitation: '73%',            //height of cloud
+                    humidity: '73%',                 //vapor in air
+                    wind: '4mph'                       //mph
+          },
+
+          Mandalay:{
+                    temp: '31°C',                     // *C
+                    precipitation: '1%',            //height of cloud
+                    humidity: '48%',                 //vapor in air
+                    wind: '11km/h'                       //mph
+          },
+
+          Yangon:{
+                    temp: '32°C',                     // *C
+                    precipitation: '4%',            //height of cloud
+                    humidity: '64%',                 //vapor in air
+                    wind: '13km/h'                       //mph
+          }
+}
+
+function cityFun(name){
+          return weatherInfo[name];
+}
+
+console.log(cityFun(cityName));
