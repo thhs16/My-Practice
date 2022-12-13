241822216d4836775f1498952125a3fa4765ed11
thhs16 <thaehtapehtarsan@gmail.com>
2022-12-13 15:32:04 +0630

    my_JS_practice

--- Javascript/30Binary_search.js
+++ Javascript/30Binary_search.js
@@ -1,25 +1,29 @@
 // This is not finished yet.
           function binarySearch(arr, len, tofind){
                     
-                    let newData=[];
-                    let mid = Math.floor(   (len-1) /2);
-                    if(tofind == arr[mid]){
-        
-                        console.log(`Your data ${tofind} has found at index ${mid}.`)
+                    // let newData=[];
 
-                    }else if(tofind < arr[mid]){
+                    let mid = Math.floor(   len /2); // mid = 50
+                    for(mid; mid>=0; mid=mid/2){
+                        if(tofind == arr[mid]){
+                            
+                            console.log(`Your data ${tofind} has found at index ${mid}.`)
+                            break;
 
-                        let negMid = -(len-1-mid);
-                        sliceObj.mySlice(negMid+1, arr, len);
-                        newData = sliceObj.newArr;
-                        console.log('newData:',newData);
+                        }else if(tofind < arr[mid]){
+
+                            let negMid = -(len-mid); //-5
+                            sliceObj.mySlice(negMid, arr, len);
+                            arr = sliceObj.newArr;
+                            console.log('newData:',arr);
                         
-                    }else{
+                        }else{
 
-                        sliceObj.mySlice(mid+1, arr, len);
-                        newData = sliceObj.newArr;
+                            sliceObj.mySlice(mid, arr, len);
+                            arr = sliceObj.newArr;
+                            console.log('newData:',arr);
+                        }
                     }
-                    
           }
 
           function lenOfArr(arr){
@@ -76,7 +80,7 @@
                                       this.index++;
                              }
                       }else{
-                             for(let i=0 ; i<len+data-1 ; i++){ // -6 | -4 --> index:1 --> i<2
+                             for(let i=0 ; i<len+data ; i++){ // -6 | -4 --> index:1 --> i<2
                                     this.newArr[this.index] = arr[i];
                                     this.index++;
                              }
@@ -85,10 +89,10 @@
           }
 
 
-let toFInd = 50;
+let toFInd = 40;
 let myArr = [10,20,30,40,50,60,70,80,90,100]; //10
 let length = lenOfArr(myArr);
 const sliceObj = new Slice;
 
-let result = binarySearch(myArr, length, toFInd);
-// console.log(result);
+let result = binarySearch(myArr, length-1, toFInd);
+console.log('length:',length);   
