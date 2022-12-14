ea0de0eadf638899d2f98875b20371fd3468d1ef
thhs16 <thaehtapehtarsan@gmail.com>
2022-12-14 22:25:29 +0630

    my_JS_practice

--- Javascript/30_2Binary_search.js
+++ Javascript/30_2Binary_search.js
@@ -1,33 +1,33 @@
-// This is not finished yet.
+
           function binarySearch(arr, len, tofind){
 
-                    // let newArray = arr;
+                    let newArray = arr;
                     let index =findIndex(tofind, arr);
 
                     let mid = Math.floor(   len /2); // mid = 50
                     for(mid; mid>=0; mid=Math.floor(len/2)){
-                        if(tofind == arr[mid]){
+                        if(tofind == newArray[mid]){
                             
                             console.log(`Your data ${tofind} has found at index ${index}.`)
                             break;
 
-                        }else if(tofind < arr[mid]){
+                        }else if(tofind < newArray[mid]){
 
                             let negMid = -(len-mid); //-5
-                            sliceObj.mySlice(negMid, arr, len);
-                            arr = sliceObj.newArr;
+                            sliceObj.mySlice(negMid, newArray, len);
+                            newArray = sliceObj.newArr;
                             // console.log('newData:',newArray);
                         
                         }else{
 
-                            sliceObj.mySlice(mid, arr, len);    // len problem  #1
-                            arr = sliceObj.newArr; // mySlice problem   #2
+                            sliceObj.mySlice(mid, newArray, len);    // len problem  #1
+                            newArray = sliceObj.newArr; // mySlice problem   #2
                             // console.log('newData:',newArray);
                         }
-                        len = lenOfArr(arr)-1;
+                        len = lenOfArr(newArray)-1;
                     }
 
-                    if(arr == 0){
+                    if(newArray == 0){
                         console.log('No results.');
                     }
           }
@@ -101,13 +101,8 @@
 
 
 let toFInd = 30;
-const myArr = [10,20,30,40,50,60,70,80,90,100]; //10
+let myArr = [10,20,30,40,50,60,70,80,90,100]; //10
 let length = lenOfArr(myArr);
 const sliceObj = new Slice;
 
 let result = binarySearch(myArr, length-1, toFInd);
-// console.log('length:',length);
-console.log(myArr);
-
-// sliceObj.mySlice(0, myArr, length-1);
-// console.log(sliceObj.newArr);
--- Javascript/31Array_clone_function.js
+++ Javascript/31Array_clone_function.js
@@ -1,7 +1,12 @@
           let myArr = [10,20,30,40,50,60,70,80,90,100];
 
+          // function cloneArr(arr){
+          //           return arr;
+          // }
+
           function cloneArr(arr){
-                    return arr;
+                    let clone = [...arr];
+                    return clone;
           }
           let newArr = cloneArr(myArr);
           console.log('newArr:',newArr);
