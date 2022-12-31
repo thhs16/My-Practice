a15f5967a97ad3329b3fae7877e26eaf03c21a13
thhs16 <thaehtapehtarsan@gmail.com>
2022-12-31 17:57:52 +0630

    my_JS_practice

--- Javascript/24cloneTest.js
+++ Javascript/24cloneTest.js
@@ -1,13 +1,25 @@
-let numOf3D = document.getElementById('total');
-// let numOf3D = document.querySelector(' input[id="total"] ');
-console.log(numOf3D.value);
-let element = document.getElementById('3D');
-// let element = document.querySelector(' input[id="3D"] ');
-
-const newClone = element.cloneNode(true);
 
 function myfun(){
+          let numOf3D = document.getElementById('total');
+
+          let list = document.getElementById('list');
+
+          let element = document.getElementById('3D');
+
+          let newClone = element.cloneNode(true);
+
           for(let i=1; i<numOf3D.value; i++){
-          element.after(newClone);
+
+          console.log('i:', i);
+          console.log('newClone:', newClone);
+
+          newClone.id = `3D`+i;
+          // element.after(newClone);
+          list.appendChild(newClone);
           }
+          // let i = 1;
+          // while(i<numOf3D.value){
+          //           i++;
+          //           list.appendChild(newClone);
+          // }
 }
