87dfc075e57553c433a101003c116575f6c41508
thhs16 <thaehtapehtarsan@gmail.com>
2023-01-04 22:07:35 +0630

    EJS_practice

---
+++ EJS/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
+
+// app.set('myfile',path.join(__dirname,'myfile'));
+
+app.set('view engine','ejs');
+
+app.get('/',function(req,res){
+          res.render('index');
+});
+
+app.listen(8080,function(){
+          console.log('Server is listening on port 8080.');
+});
