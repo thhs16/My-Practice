72f2709e5021e5158d5ef69e763f3d99233b4b7b
thhs16 <thaehtapehtarsan@gmail.com>
2023-01-05 16:20:00 +0630

    3D_lottery

--- Javascript/24_3D_lottery.js
+++ Javascript/24_3D_lottery.js
@@ -1,18 +1,19 @@
 let clas = 2;
+let ele  = document.querySelector('.D31');
 
 function myfun(){
           let list = document.getElementById('list');
-          var ele  = document.querySelector('input[class="3D1"]');
-          // var ele  = document.getElementsByName('3D1'); //doesn't work
-          // let ele  = document.getElementsByClassName('3D'); //doesn't work
+          // var ele  = document.getElementsByName('3D1'); //doesn't work due to naming convention 
+          // let ele  = document.getElementsByClassName('3D'); //doesn't work due to naming convention
           let clon = ele.cloneNode(true);
 
                     console.log('ele',ele);
                     console.log('clone:',clon);
                     console.log('class:',clas);
 
-          clon.className = `3D${clas}`;
-          clon.name      = `3D${clas}`;
+          clon.className = `D3${clas} D3__spaces`;
+          clon.name      = `D3${clas}`;
+          clon.value     = '';
           clas++;
 
                     console.log('class after changes :',clas);
@@ -25,28 +26,31 @@ function myfun(){
           list.appendChild(clon);
 }
 
-// ele.addEventListener('input', checking());
+ele.addEventListener('blur', checking());
 
 function checking(){
+          let D3__inputs = document.querySelectorAll('.D3__spaces');
+                    console.log('D3__inputs: ',D3__inputs);
+                    console.log('length: ',D3__inputs.length);
+          D3__inputs.addEventListener('input', childChecking(D3__inputs.length));
+          
 
-          for(let i=2; i<clas; i++){
-                    // let check3Dst = document.getElementsByName(`3D${i-1}`);
-                    let check3Dst = document.querySelector('input[name="3D1"]');
-                    // let check3Dnd = document.getElementsByName(`3D${i}`);
-                    let check3Dnd = document.querySelector('input[name="3D2"]');
+          function childChecking(len){
+                    for(let i=2; i<clas; i++){
+                              let check3Dst = document.querySelector(`.D3${i-1}`);
+
+                              let check3Dnd = document.querySelector(`.D3${i}`);                    
+
+                              console.log('check3Dst:',check3Dst);
+                              console.log('check3Dnd:',check3Dnd);
                     
-
-
-                    console.log('check3Dst:',check3Dst);
-                    console.log('check3Dnd:',check3Dnd);
-                    
-                    if(check3Dst.value == check3Dnd.value){
-                              check3Dnd.title = 'This number has already chosen.'
-                              check3Dnd.style.border = "1px solid red";
-                    }else{
-                             check3Dnd.title = 'must be 3 digit';
-                             check3Dnd.style.border = "2px solid black"; 
+                              if(check3Dst.value == check3Dnd.value){
+                                        check3Dnd.title = 'This number has already chosen.';
+                                        check3Dnd.style.border = "1px solid red";
+                              }else{
+                                        check3Dnd.title = 'must be 3 digit';
+                                        check3Dnd.style.border = "2px solid black"; 
+                              }
                     }
-
           }
 }
