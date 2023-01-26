2940443ab8754633dcd57fc30a4f6caee7490106
thhs16 <thaehtapehtarsan@gmail.com>
2023-01-26 17:44:04 +0630

    3D_lottery

--- Javascript/24_3D_lottery2.js
+++ Javascript/24_3D_lottery2.js
@@ -1,7 +1,7 @@
 // (fixed)first attempt don't show red border but second try of focusout works
 // (fixed)current checking scheme : check 3d focusing by looking at previous 3d space
 // (done)style : outline
-//  to check the value when client add the input as soon as it has generated
+// (done)to check the value when client add the input as soon as it has generated
 // focusout-outline : black if value is available
 // 3digit checking
 
@@ -9,7 +9,7 @@ let clas = 1;
 let ele = document.querySelector('.D31');
 let genButt = document.querySelector('#gen');
 
-ele.addEventListener('focusout', checkingfun1);
+// ele.addEventListener('focusout', checkingfun1);
 // genButt.addEventListener('click', checkingfun1);
 
 function myfun() {
@@ -36,6 +36,7 @@ function myfun() {
                     return 0;
           }
           list.appendChild(clon);
+          checkingfun1();
 
 }
 
