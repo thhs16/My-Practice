0b077beb74cae4572e79b73e4cbffc555f6381f2
thhs16 <thaehtapehtarsan@gmail.com>
2023-03-08 18:05:34 +0630

    3D_lottery

--- Javascript/24_3D_server.js
+++ Javascript/24_3D_server.js
@@ -1,3 +1,5 @@
+// how to access value of var from another js file
+
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -13,4 +15,16 @@ app.use(express.static(path.join(__dirname,'/public')));
 
 app.listen(8080, ()=>{
           console.log('Server is starting at port:8080');
+})
+
+app.post("/form_3D",(req,res)=>{
+          var i=1;
+
+          var usname = req.body.usName;
+          var phno = req.body.phone;
+          do{
+          var num3D${i} = req.body.D3${i};
+          i++;
+          }
+          while(i);
 })
