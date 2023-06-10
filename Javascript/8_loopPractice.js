5f479610c193dc8400a8d1bc7fcf716b3bd68756
thhs16 <thaehtapehtarsan@gmail.com>
2023-06-10 19:33:53 +0630

    JS_practice

---
+++ Javascript/8_loopPractice.js
@@ -0,0 +1,11 @@
+
+          for(let i=0; i<10; i++){
+
+                    for(let z=0; z<i; z++){
+
+                              console.log(`${"*"}`);
+                    }
+                    console.log("\n");
+          }
+
+          // the output is not quite right yet.
---
+++ Javascript/8_whileLoop.js
@@ -0,0 +1,13 @@
+// always want to run programme? Use while loop.
+// work at least once even the condition is not true.
+
+
+let a = 0;
+let b = 10;
+let c = 0;
+
+          while(a<b){
+                    c++;
+                    a = a+2;
+                    console.log("a is equal to b",c);
+          }
