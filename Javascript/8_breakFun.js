a76cfb4be69c452376c06d6e88651ba787bbb3d5
thhs16 <thaehtapehtarsan@gmail.com>
2023-06-11 22:28:19 +0630

    JS_practice

---
+++ Javascript/8_breakFun.js
@@ -0,0 +1,16 @@
+let a = 0;
+let b = 10;
+let c = 0;
+let d = 10
+
+          while(a<b){
+                    console.log("a is greater than to b",b);
+                    b--;
+                    while(c<d){
+                              if(c == 5){
+                                        break;
+                              }
+                    console.log("hello")
+                    c++;
+                    }
+          }
--- Javascript/8_whileLoop.js
+++ Javascript/8_whileLoop.js
@@ -1,5 +1,4 @@
 // always want to run programme? Use while loop.
-// work at least once even the condition is not true.
 
 
 let a = 0;
@@ -7,7 +6,8 @@ let b = 10;
 let c = 0;
 
           while(a<b){
-                    c++;
-                    a = a+2;
-                    console.log("a is equal to b",c);
+                              c++;
+                              a = a+2;
+                    
+               console.log("a is equal to b",c);
           }
