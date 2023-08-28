3904e981d68e4b8aa973d50951f0725aa962fac5
thhs16 <thaehtapehtarsan@gmail.com>
2023-08-28 16:52:28 +0630

    JS_practice

---
+++ Javascript/13_object2.js
@@ -0,0 +1,20 @@
+// bracket notation
+// JS object notation
+
+const object_name = {
+          name : "THHS",
+          age : 18,
+          hobby : "programming",
+
+          fun : function(){
+                  return 0;
+          }
+
+}
+
+          object_name["name"] = "Ivy";
+          object_name["hobby"] = "Research"; 
+
+  console.log(object_name["name"]);
+  console.log(object_name["hobby"]);
+  object_name[fun]();
