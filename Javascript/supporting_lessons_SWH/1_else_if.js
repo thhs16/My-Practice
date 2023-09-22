4bbc440e2b109f190be520f246476533898d6f74
thhs16 <thaehtapehtarsan@gmail.com>
2023-09-23 01:05:49 +0630

    Learning_JS

---
+++ Javascript/supporting_lessons_SWH/1_else_if.js
@@ -0,0 +1,18 @@
+// allow many options
+ let foods = "";
+
+ if(foods === "noodle" ) {
+
+          document.writeln("Customer likes noodle.");
+ } else if(foods === "chicken") {
+
+          document.writeln("Customer likes chicken.");
+ } else if(foods === "coffee") {
+
+          document.writeln("Customer likes coffee.");
+ } else {
+          
+          document.writeln("Special customer");
+
+ } // 4 conditions
+  
